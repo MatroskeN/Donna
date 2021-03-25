42b4d39173a93103d6564de6873375942eaee552
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2021-03-25 12:02:06 +0400

    fef

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,18 @@ var swiper = new Swiper('.swiper-container', {
       snapOnRelease: false,
     },
   });
+
+  //modal
+  $("#burger").on("click", function () {
+    document.getElementById("burger").style.display = "none";
+    document.getElementById("xclose").style.display = "block";
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("body").style.overflow = "hidden";
+});
+
+$("#xclose").on("click", function () {
+    document.getElementById("burger").style.display = "block";
+    document.getElementById("xclose").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("body").style.overflow = "visible";
+});
